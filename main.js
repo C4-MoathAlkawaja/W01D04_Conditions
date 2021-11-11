dd3ec4b4388ebf810ec1d2979122ac9b274fb1f1
MoathAlkawaja <93875570+MoathAlkawaja@users.noreply.github.com>
2021-11-11 16:54:40 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+console.log(arrowTitle)
+/* START CODE UNDER THIS LINE */
+// Pulse Check 
+//Q1)Figure out the syntax errors in the following, and fix them.
+
+if(10<=10){}
+
+if (5>3){}
+
+if (5>10){}
+else(true)
+
+if (2<3&&7>10){}
+
+
+if("hello"=="heloo"){} // == or ===
+
+//Q2) predic the value of the following expressions and explin why
+
+//1- the answer is false can we have 2 true and 1 false 
+
+//2- the answer is true ------true or (false) equal true 
+
+//3- (5 === 2) = false 
+//false or true = true 
+
+//4 - (true && true) || (false && true)
+            // (true)||(false)
+
+    //the answer is false 
+
+
+    //Q3) const legalAge = function (age) {  if(age>=18 )return "true"}
+
+    //else{return "false"}
+
+//Q4) write a function is It My Name 
+
+const isItMyName = function (name) 
+ {
+     if (name=="moath"){
+         return "true"
+     }
+
+else 
+return "false"}
+
+// Q5)
+ const positiveOrNegative = function(number) {
+    if (number>0){
+    return "number is positive"}
+else 
+if(number<0){
+ return "number is negative"}
+
+else return "is neither positive nor negatine"}
+
+
+
+//practice
+
+//Q1) Write a function toLowerOrUpperCase that accepts two arguments, string and strCase, and returns the string in lower case or upper case depending on what was specified in the second parameter. Check out the following builtin methods then solve the question.
+
+const toLowerOrUpperCase =  function (satring, strCase) {
+
+
+    
+}
